a661cf3a23064d0d361ac524f74a4543660e9653
miagrafico <36642088+miagrafico@users.noreply.github.com>
2019-01-24 08:25:22 -0500

    Final Angular

---
+++ js/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/* App Module */
+/* se crea el modulo del aplicativo*/
+var carApp = angular.module('carApp', [
+	'ngRoute',
+  'carAppAnimations',
+  'carAppControllers',
+  'carAppServices' // se agrega el servicio creado en services.js
+]);
+
+
+//Se agrega la configuracion de rutas!!
+carApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/cars', {
+        templateUrl: 'partials/car-list.html',
+        controller: 'CarListCtrl'
+      }).
+      when('/cars/:carId', {
+        templateUrl: 'partials/car-detail.html',
+        controller: 'CarDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/cars'
+      });
+  }]);
